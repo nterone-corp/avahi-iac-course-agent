6fc9c504d7da767ddf837f9de717d3bb5da65a0e
OscarAvahi <oscar.segura@avahitech.com>
2025-11-28 11:23:51 -0600

    FEAT(sql_policies.sql): Added ai_safe_events

--- SQL policies.sql
+++ SQL policies.sql
@@ -5,10 +5,13 @@ public_featured_events TO ai_agent_user;
 CREATE VIEW ai_safe_users AS SELECT id,
 first_name, last_name, email, company_id, origin_region, active_regions, created_at,
 updated_at, archived, active FROM users;
+CREATE VIEW ai_safe_courses AS SELECT id, slug, title, abbreviation, platform_id, price, origin_region,
+active_regions, created_at, updated_at, option_for, developed_by_nterone, search_terms
+FROM courses
 CREATE VIEW ai_safe_events AS SELECT id, start_date, end_date, format, price, course_id,
 guaranteed, active, start_time, end_time, city, state, status, public, language, time_zone,
 archived, approved, max_student_count, origin_region, active_regions FROM events;
-GRANT SELECT ON ai_safe_users, ai_safe_events TO ai_agent_user;
+GRANT SELECT ON ai_safe_users, ai_safe_events, ai_safe_courses TO ai_agent_user;
 
 ALTER USER ai_agent_user CONNECTION LIMIT 5;
 ALTER USER ai_agent_user SET statement_timeout = '30s';
